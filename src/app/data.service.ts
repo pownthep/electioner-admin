64c73279181e4d894ce3593b87048b1e9e5abb71
Pownthep <pownthepofficial@gmail.com>
2019-02-27 03:27:21 +0700

    Last commit

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -8,7 +8,7 @@ import { Representative } from './representative';
   providedIn: 'root'
 })
 export class DataService {
-  private baseUrl = "https://electioner.tk";
+  private baseUrl = "http://localhost:8080";
 
   constructor(private http: HttpClient) { }
 
